7d2c8b4beffb169c1bc052d64b5b63d9d1ac98cc
sureshpadala2705 <sureshpadala83@gmail.com>
2023-05-08 21:42:30 +0530

    4

--- salesforce.sh
+++ salesforce.sh
@@ -4,10 +4,7 @@ working in developmemt branch
 hi 
 I am updating this files on8 may
 in the master branch
-<<<<<<< HEAD
 working in the cherry command
 working in the dveops technologies`
-=======
 working in the cherry command`
 testing the merge and cherry
->>>>>>> 3d67cb3 (third)
